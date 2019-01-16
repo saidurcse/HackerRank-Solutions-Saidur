045200c624936153773e712c6ba1d1731cbba82a
saidurcse <saidursd@gmail.com>
2019-01-16 22:34:40 +0600

    new solution

---
+++ Algorithm/A Very Big Sum.java
@@ -0,0 +1,60 @@
+https://www.hackerrank.com/challenges/a-very-big-sum/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the aVeryBigSum function below.
+    static long aVeryBigSum(long[] ar) {
+
+        long result = 0;
+        for(int i=0;i<ar.length;i++){
+            result = result + ar[i];
+        }
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long[] ar = new long[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            long arItem = Long.parseLong(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        long result = aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
+Sample Input:
+
+5
+1000000001 1000000002 1000000003 1000000004 1000000005
+
+Sample Output:
+
+5000000015
